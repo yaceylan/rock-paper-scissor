534301817e9cf1dad7a6513960770d6b8eac9964
Yasemin Ceylan <yaceybln@gmail.com>
2025-01-14 20:27:12 +0100

    added variable declarations

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const startScreen = document.getElementById('startscreen');
+const startButton = document.getElementById('startbutton');
+const rulesButton = document.getElementById('rulesbutton');
+const scoreElement = document.getElementById('score');
+const resultsElement = document.getElementById('results');
+const choices = document.querySelectorAll('button[data-choice]');
+
+
+
+
